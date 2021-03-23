8e3a71e320012d9d8561b7ea0f91a757f196937e
JoeGreenwood <thesmackherald@gmail.com>
2021-03-23 16:41:55 -0400

    Added arrays to js.

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var passwordLength = prompt("Enter the length of your password")
+  // if statement to determine that length is 8 - 128
+
+  var hasSpecialCharacters = confirm("Include number")
+
+  return
+}
+
+//Remember to create arrays for the characters being used
+//Look at how to get the characters out of the array
+//Use for loop to go through characters
+// Use math random methods
+
+window.alert("This is an alert");
+
+var upperCaseText = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var lowerCaseText = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var numbersZeroToNine = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "'", ",", "<", ".", ">", "/", "?"]
+
