9517613e64c32577f7b14ec29f20592d59d99d05
JoeGreenwood <thesmackherald@gmail.com>
2021-03-25 09:47:10 -0400

    Added randomizer and user prompts

--- script.js
+++ script.js
@@ -7,34 +7,62 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+                         // Pretty sure I enter the password into here.
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
 
-  var passwordLength = prompt("Enter the length of your password")
-  // if statement to determine that length is 8 - 128
 
-  var hasSpecialCharacters = confirm("Include number")
+// These prompts determine the character make-up and length of the desired password.
+var passwordLength = prompt("Enter a number between 8 and 128")
 
-  return
-}
+var hasLower = prompt("Will this password have lower-case letters?")
 
-//Remember to create arrays for the characters being used
-//Look at how to get the characters out of the array
-//Use for loop to go through characters
-// Use math random methods
+var hasUpper = prompt("Will this password have upper-case letters?")
 
-window.alert("This is an alert");
+var hasNumber = prompt("Will this password contain numbers")
 
-var upperCaseText = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var hasSpecial = prompt("Will this password contain special characters?")
 
-var lowerCaseText = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var numbersZeroToNine = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+// If the user says yes to has lower, need to run ramdom lower-case function.
+// If the user says yes to has uppper, need to run ramdom upper-case function.
+// If the user says yes to has special characters, need to run random special function.
+// Create a function for loop.
+// Characters produced by randomonium are pushed into an array.
+// Turn array into a string, defined length set by password length prompt.
+// If statements for prompts.
+// Try to change randomonium into an array.
 
-var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "'", ",", "<", ".", ">", "/", "?"]
 
+// This generates a random lower-case character.
+// It works by producing a decimal, multiplying the result by 26 and then adding 97. This is compared to the UTF-8 charset and a random character is chosen by it's numeric designation.
+function randomLowerCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+// console.log(randomLowerCase());
+
+// This follows the same process as the random lower-case, but starts selecting characters after UTF-8 position 65.
+function randomUpperCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+// console.log(randomUpperCase());
+
+// This function works the same as the two above, but with a string length of 10 to catch the numbers 0-9 that start from position 48 of the charset.
+function randomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+    }
+// console.log(randomNumber());
+
+// This one is a little different. It establishes a string as a constant, then treats it as an array as it establishes a rounded down decimal no greater than the length of the string. Thereby returning a random special character.
+function randomSpecial() {
+    const special = "~!@#$%^&*()-+={}[];:<>,./?"
+    return special[Math.floor(Math.random() * special.length)]
+    }
+// console.log(randomSpecial());
+
+// This is an object created to store the returns from the randomizers.
+var randoMonium = [randomLowerCase(), randomUpperCase(), randomNumber(), randomSpecial()]
+console.log(randoMonium);
